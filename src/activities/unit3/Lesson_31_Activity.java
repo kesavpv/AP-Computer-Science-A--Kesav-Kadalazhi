3812b8cc3ce8b313a95dd554c14877abc76d3c86
kesav <kkadalazhi1@pvlearners.net>
2017-02-16 08:56:45 -0700

    finish lesson 31

--- src/activities/unit3/Lesson_31_Activity.java
+++ src/activities/unit3/Lesson_31_Activity.java
@@ -8,9 +8,29 @@ public class Lesson_31_Activity
 	{
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("Enter a number in base 8:");
-		int num = Integer.parseInt(scan.next(), 8);
 		
-		System.out.println(num);
+		String word;
+		int num = 0;
+		
+		System.out.println("Enter a number in base 8:");
+		word = scan.next();
+		
+		
+		if(word.length() > 8)
+		{
+			System.out.println("ERROR: Incorrect Octal Format");
+			return;
+		}
+		else
+		{
+			try {
+				num = Integer.parseInt(word, 8);
+			} catch(NumberFormatException exception) {
+				System.out.println("ERROR: Incorrect Octal Format");
+			}
+		}
+		
+		if(num != 0)
+			System.out.println(num);
 	}
 }
